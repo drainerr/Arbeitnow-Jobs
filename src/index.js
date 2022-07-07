10b29c3df186badb2c325d1176bb5e6bdf68100f
drainerr <george.kvrivi@gmail.com>
2022-07-07 14:59:50 +0400

    add 404.html

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import JobsContext from './store/JobsContext';
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <JobsContext>
-        <App />
-      </JobsContext>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<BrowserRouter basename='/Arbeitnow-Jobs'>
+			<JobsContext>
+				<App />
+			</JobsContext>
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
